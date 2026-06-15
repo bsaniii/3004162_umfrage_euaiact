cfdad14baf620cf33822ee5488cc414e1b4546c5
bsaniii <128913694+bsaniii@users.noreply.github.com>
2026-06-15 16:36:18 +0200

    Add mongoose dependency to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const ExcelJS = require('exceljs');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
